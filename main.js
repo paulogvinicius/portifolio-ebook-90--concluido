740828bdf105a4d8d8c50b93caacae8824a7dc78
Paulo Vinícius da Silva <129632438+paulogvinicius@users.noreply.github.com>
2024-02-05 16:43:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//função acessar todos os projetos ao clicar no button "ver mais"
+
+function project(){
+    window.location.href="projetos.html"
+}
+
+//função back/voltar ao clicar o button
+
+function back(){
+    window.location.href="index.html"
+}
